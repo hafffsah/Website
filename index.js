343a8fa95e56264b4523fc388ffe01fa2c479932
Tauseef Ahmad <itauseefahmad@yahoo.com>
2020-10-03 21:39:34 +0530

    Added bootstrap and also added router to index.js

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
 const PORT = process.env.PORT || 8080;
+const indexRouter = require("./routes/index.js");
 
 app.engine("handlebars", handlebars());
 app.set("view engine", "handlebars");
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.get("/", indexRouter);
 
 app.listen(PORT, () => console.log(`Started server at port ${PORT}`));
